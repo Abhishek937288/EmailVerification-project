4fc035cd881bd608cd2fad4fb204fbe7c147255e
Ubeyid Oumer <ubeyidah@gmail.com>
2025-08-09 20:30:32 +0300

    fix: use port 8080 by default

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import cookieParser from "cookie-parser";
 configDotenv();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 const frontendUrl = process.env.FRONTENDURL;
 
 app.use(express.json());

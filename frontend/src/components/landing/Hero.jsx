b6452b199e0e66146c776a1dd42f9154b7eaa559
Ubeyid Oumer <ubeyidah@gmail.com>
2025-08-09 22:38:13 +0300

    feat: add gradent bg

--- frontend/src/components/landing/Hero.jsx
+++ frontend/src/components/landing/Hero.jsx
@@ -3,7 +3,15 @@ import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <section className="h-screen flex items-center justify-center">
+    <section className="h-screen flex items-center justify-center relative">
+      <div
+        className="absolute inset-0 -z-10"
+        style={{
+          background:
+            "radial-gradient(125% 125% at 50% 10%, #fff 40%, #6366f1 100%)",
+        }}
+      />
+
       <div className="flex-1 max-w-lg text-center">
         <Heading size={"8"}>Secure & Seamless Authentication</Heading>
         <div className="my-2"></div>

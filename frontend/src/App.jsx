54c0e5e5019d651d37da1b80eae73edb07a319d3
Ubeyid Oumer <ubeyidah@gmail.com>
2025-08-09 21:11:27 +0300

    feat: create navbar and setup radix ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,18 @@
 import "./App.css";
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Route, Routes, Navigate, Outlet } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Contact from "./pages/Contact/Contact";
 import About from "./pages/About/About";
 import Signup from "./pages/Signup/Signup";
-import RootLayout from "./Layouts/layouts";
 import { getAuth } from "./Util/auth";
 import PublicLayout from "./Layouts/Public";
 import VerifyOtp from "./components/VerifyOtp/VerifyOtp";
 import Authlayout from "./Layouts/Authlayout";
-/*
- - i think i should start the refactor code instead of just comment here i think
-*/
+import RootLayout from "./Layouts/RootLayout";
+
 function MainLayout() {
-  // seprate every component in its own files
-  const auth = getAuth(); // checkingthe auth is good palce here but if it is comming or loading show loading ui here before redirect
+  const auth = getAuth();
   if (!auth) {
     return <Navigate to={"/signup"} />;
   }
@@ -30,20 +26,18 @@ function MainLayout() {
 function App() {
   return (
     <div className="app">
-      <ToastContainer />
       <Routes>
-        <Route element={<MainLayout />}>
-          <Route element={<RootLayout />}>
+        <Route element={<RootLayout />}>
+          <Route element={<MainLayout />}>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
           </Route>
-        </Route>
-        <Route element={<PublicLayout />}>
-          <Route element={<Authlayout />}>
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/verifyotp/:email" element={<VerifyOtp />} />
-            //verify-otp kebab-case
+          <Route element={<PublicLayout />}>
+            <Route element={<Authlayout />}>
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/verifyotp/:email" element={<VerifyOtp />} />
+            </Route>
           </Route>
         </Route>
       </Routes>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,22 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {BrowserRouter} from "react-router-dom";
-import {UserProvider} from './context/userContext';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from "./context/userContext";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
+import { ToastContainer } from "react-toastify";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <UserProvider>
-    <App />
-    </UserProvider>
+      <UserProvider>
+        <Theme>
+          <ToastContainer />
+          <App />
+        </Theme>
+      </UserProvider>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 );
